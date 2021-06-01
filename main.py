46be8d137e898c3bf690a7c335c9efce02d0f539
cyanbox <cyanbox@zju.edu.cn>
2021-06-01 11:26:47 +0800

    Add sketch colorizor (with bugs)

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import numpy as np
 import cv2
 from local_tinter import RecRegion, colorize_inside_region
 from swatch_colorizor import colorize_photo
+from sketch_colorizor import colorize
 
-img = cv2.imread("4.jpg")
-ref = cv2.imread("3.png")
+# img = cv2.imread("4.jpg")
+# ref = cv2.imread("3.png")
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_swatch = RecRegion(0, 0, 200, 50)
-ref_swatch = RecRegion(0, 0, 350, 100)
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img_swatch = RecRegion(0, 0, 200, 50)
+# ref_swatch = RecRegion(0, 0, 350, 100)
 
-img_swatch2 = RecRegion(0, 200, 200, 50)
-ref_swatch2 = RecRegion(0, 300, 350, 100)
+# img_swatch2 = RecRegion(0, 200, 200, 50)
+# ref_swatch2 = RecRegion(0, 300, 350, 100)
 
-img_l = [img_swatch, img_swatch2]
-ref_l = [ref_swatch, ref_swatch2]
+# img_l = [img_swatch, img_swatch2]
+# ref_l = [ref_swatch, ref_swatch2]
 
-colorize_photo(img, ref, img_l, ref_l)
+# colorize_photo(img, ref, img_l, ref_l)
+
+img = cv2.imread("gray.jpg")
+sketch = cv2.imread("sketch.bmp")
+# cv2.imshow("title", img)
+# cv2.waitKey(0)
+
+# cv2.imshow("title", sketch)
+# cv2.waitKey(0)
+colorize(img, sketch)
---
+++ sketch_colorizor.py
@@ -0,0 +1,84 @@
+from operator import ne
+import cv2
+import numpy as np
+from scipy import sparse
+from sklearn.preprocessing import normalize
+
+def colorize(img, sketch):
+    # transfer from BGR to YUV
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    sketch = cv2.cvtColor(sketch, cv2.COLOR_BGR2YUV)
+
+    img = img.astype(np.float64)
+    sketch = sketch.astype(np.float64)
+
+    # prepare for output
+    output = np.zeros(img.shape, dtype=np.float64)
+    output[0] = img[0]
+
+    def cal_index(img, p):
+        row_num = img.shape[0]
+        return p[0] * row_num + p[1]
+
+    def get_neighbors(img, p, d=3):
+        y = p[0]
+        x = p[1]
+        left = max(0, x - d)
+        right = min(img.shape[1] - 1, x + d)
+        top = max(0, y - d)
+        bottom = min(img.shape[0] - 1, y + d)
+        neighbors = []
+        for i in range(top, bottom + 1):
+            for j in range(left, right + 1):
+                if not (i == y and j == x):
+                    neighbors.append((i, j))
+        neighbors.append((y, x))
+        return neighbors
+    
+    def get_weights(img, p):
+        neighbors = get_neighbors(img, p)
+        img_lum = img[:, :, 0]
+        neighbor_idx = [cal_index(img, p) for p in neighbors]
+        neighbor_lum = [img_lum[p[0], p[1]] for p in neighbors]
+        neighbor_std = np.std(neighbor_lum)
+        
+        # some special tricks for std
+        sigma = neighbor_std * 0.75
+        mgv = min((neighbor_lum - img_lum[p[0], p[1]]) ** 2)
+        sigma = max(sigma, -mgv / np.log(0.01))
+        sigma = max(sigma, 0.000002)
+        neighbor_weights = np.exp(-(neighbor_lum - img_lum[p[0], p[1]]) ** 2 / (2 * (sigma) ** 2))
+        neighbor_weights[-1] = 0.
+        neighbor_weights = neighbor_weights / np.sum(neighbor_weights)
+        neighbor_weights[-1] = 1.
+        return neighbor_weights, neighbor_idx
+    
+    def get_weight_matrix(img):
+        n, m = img.shape[0], img.shape[1]
+        weight_matrix = sparse.lil_matrix((n * m, n * m))
+        print(weight_matrix.shape)
+
+        for i in range(n):
+            for j in range(m):
+                neighbor_weights, neighbor_idx = get_weights(img, (i, j))
+                weight_matrix[cal_index(img, (i, j)), neighbor_idx] = neighbor_weights
+        weight_matrix[np.arange(n * m), np.arange(n * m)] = 1.
+        return weight_matrix
+    
+    weight_matrix = get_weight_matrix(img)
+    colored_region = sketch.sum(2) > 2
+    colored_indices = np.nonzero(colored_region.reshape(img.shape[0] * img.shape[1], order='F'))
+
+    for channel in [1, 2]:
+        colored_img = sketch[:, :, channel].flatten()
+        b = np.zeros(img.shape[0] * img.shape[1])
+        b[colored_indices] = colored_img[colored_indices]
+        x = sparse.linarg.spsolve(weight_matrix, b)[:img.shape[0]*img.shape[1]]
+        output[:, :, channel] = x.reshape(img.shape[0], img.shape[1])
+    
+    output = output.astype(np.uint8)
+    output = cv2.cvtColor(output, cv2.COLOR_YUV2BGR)
+
+    cv2.imshow("title", output)
+    cv2.waitKey(0)
+        
